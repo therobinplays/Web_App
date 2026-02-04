dcaada56389eac26f9bbad6371eb9c384dfad92b
therobinplays <therealrobinplys>
2026-02-04 15:39:15 +0545

    kuch bhi

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,29 +1,33 @@
 function Register() {
-    return (
-        <div>
+  return (
 
-        <h1 >Register</h1><br />
+    <div className="min-h-screen flex items-center justify-center bg-rose-50">
 
-        <form>
+      <form className="bg-white p-8 rounded-lg shadow-md w-80">
 
-        <label htmlFor="fname">First name:</label><br />
-        <input type="text" id="fname" name="fname" /><br />
+        <h1 className="text-2xl font-bold text-center text-rose-600 mb-6">Register</h1>
 
-        <label htmlFor="lname">Last name:</label><br />
-        <input type="text" id="lname" name="lname" /><br />
+        <label className="block font-bold text-black-700 mb-1" htmlFor="fname">First Name</label>
+        <input placeholder="Saubhagya" className="w-full mb-4 px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-rose-400" type="text" id="fname" name="fname" />
 
-        <label htmlFor="email">Email:</label><br />
-        <input type="email" id="email" name="email" /><br />
+        <label className="block font-bold text-black-700 mb-1" htmlFor="lname">Last Name</label>
+        <input placeholder="Timilsina" className="w-full mb-4 px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-rose-400" type="text" id="lname" name="lname" />
 
-        <label htmlFor="password">Password:</label><br />
-        <input type="password" id="password" name="passwod" /><br />
+        <label className="block font-bold text-black-700 mb-1" htmlFor="email">E-mail</label>
+        <input placeholder="saubhagyatimilsina@gmail.com" className="w-full mb-4 px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-rose-400" type="email" id="email" name="email" />
 
-        <button type="submit">Submit</button>
+        <label className="block font-bold text-black-700 mb-1" htmlFor="password">Password</label>
+        <input placeholder="Hello@12345" className="w-full mb-6 px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-rose-400" type="password" id="password" name="password" />
 
-        </form>
+        <label className="block font-bold text-black-700 mb-1" htmlFor="password">Date</label>
+        <input className="w-full mb-6 px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-rose-400" type="date" id="date" name="date" />
+
+        <button className="w-full bg-rose-500 text-white py-2 rounded hover:bg-rose-600 transition" type="submit" > Submit </button>
+      
+      </form>
 
     </div>
-    );
+  );
 }
 
 export default Register;
